0dc4df488043639654031672e2e17058ea6acf64
Abhishek sharma <abhisheksharma73580@gmail.com>
2023-09-13 20:48:37 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,31 @@ import { useState, useCallback } from 'react'
 import './App.css'
 
 function App() {
-    const [length, setLength] = useState(8);
-    const [numberAllowed, setNumberAllowed] = useState(false);
-    const [charAllowed, setCharAllowed] = useState(false);
-    const [password, setPassword] = useState("")
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("")
 
-    const passwordGenerator = useCallback(() =>{
-      let pass = "";
-      let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcbefghijklmnopqrstuvwxyz";
-      if(numberAllowed) str += "0123456789";
-      if(charAllowed) str += "~`!@#$%^&*()_+{}|:[]\="
-    }, [length, numberAllowed, charAllowed, setPassword]);
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcbefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "~`!@#$%^&*()_+{}|:[]\="
 
+    for(let i = 1; i <= Array.length; i++){
+        let char = Math.floor(Math.random() * str.length + 1)
+        pass = str.charAt(char)
+    }
+    setPassword(pass)
+
+  }, [length, numberAllowed, charAllowed, setPassword]);
 
   return (
     <>
-      <p className='text-4xl text-center'>Password Generator</p>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg my-8 text-orange-500 bg-gray-700 '>
+          Abhishek sharma
+      </div>
     </>
   )
-  }
-
+}
 export default App
